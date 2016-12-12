393abefc7788c992901cee7d2028e39eb2241bcc
Richard Surjadi <richsurjadi@icloud.com>
2016-12-12 23:25:10 +1100

    Added Task Class and tasks with important labels

---
+++ To Do/Task.swift
@@ -0,0 +1,14 @@
+//
+//  Task.swift
+//  To Do
+//
+//  Created by Richard Surjadi on 12/12/16.
+//  Copyright © 2016 Richard Surjadi. All rights reserved.
+//
+
+import Foundation
+
+class  Task {
+    var name = ""
+    var important = true
+}
--- To Do/ViewController.swift
+++ To Do/ViewController.swift
@@ -12,23 +12,49 @@ class ViewController: UIViewController, UITableViewDataSource,UITableViewDelegat
 
     @IBOutlet weak var tableView: UITableView!
     
+    var tasks : [Task] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+     
+        tasks = makeTasks()
         
         tableView.dataSource = self
         tableView.delegate = self
     }
 
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 10
+        return tasks.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell()
-        cell.textLabel?.text = "Hey!"
+        let task = tasks[indexPath.row]
+        if task.important {
+            cell.textLabel?.text = "\(task.name) ‼️"
+        } else {
+            cell.textLabel?.text = "\(task.name)"
+        }
+
         return cell
     }
 
+    func makeTasks() -> [Task] {
+        let task1 = Task()
+        task1.name = "Walk the dog"
+        task1.important = false
+        
+        let task2 = Task()
+        task2.name = "Pick up Sabby's present"
+        task2.important = true
+        
+        let task3 = Task()
+        task3.name = "Dinner"
+        task3.important = true
+        
+        return [task1, task2, task3]
+    }
+    
 }
 
